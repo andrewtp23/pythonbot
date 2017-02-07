5f9a51c57a3abaeea61cd9a47a0742107c0c9313
andrewtp23 <atp23@zips.uakron.edu>
2017-02-06 21:40:16 -0500

    got cleverbot to work

--- mybot.py
+++ mybot.py
@@ -1,8 +1,10 @@
 import discord
 import asyncio
 import json
+from cleverbot import Cleverbot
 
 client = discord.Client()
+cb = Cleverbot('bot')
 
 with open('mybot.json') as json_data:
      d = json.load(json_data)
@@ -13,20 +15,25 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
+   if not message.author.bot:   
+        if message.content.startswith('!test'):
+           counter = 0
+           tmp = await client.send_message(message.channel, 'Calculating messages...')
+           async for log in client.logs_from(message.channel, limit=100):
+              if log.author == message.author:
+                 counter += 1
 
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+              await client.edit_message(tmp, 'You have {} messages.'.format(counter))
 		
-    if message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
+        if message.content.startswith('!sleep'):
+            await asyncio.sleep(5)
+            await client.send_message(message.channel, 'Done sleeping')
 
-    if d[message.content]:
-        await client.send_message(message.channel, d[message.content])
+        if client.user in message.mentions:
+            await client.send_message(message.channel, cb.ask(message.content) )	
+		
+        if d[message.content]:
+            await client.send_message(message.channel, d[message.content])
 
+        
 client.run('MjY4ODQxMzUwODgxODY5ODI0.C1gpig.ao-9PcunLls7Hf7DorYp0hhHAfc')
